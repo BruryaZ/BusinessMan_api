657f6b9c2a86b4420f99e1c9f2060edda1c1833f
Brurya-Zarbiv <bz0556758244@gmail.com>
2025-03-13 14:39:55 +0200

    User app arrangement

--- users_app/src/app/app.component.ts
+++ users_app/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UploadFileComponent } from '../componenets/upload-file/upload-file.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UploadFileComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- users_app/src/app/app.routes.ts
+++ users_app/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
-import { UploadFileComponent } from '../componenets/upload-file/upload-file.component';
+import { UploadFileComponent } from './componenets/upload-file/upload-file.component';
+import { LoginComponent } from './componenets/login/login.component';
 
 export const routes: Routes = [
-    {path:'upload-file', component: UploadFileComponent},
+    { path: 'upload-file', component: UploadFileComponent },
+    { path: '', component: LoginComponent },
+    { path: 'login', component: LoginComponent },
 ];
